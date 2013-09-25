34432b62da6926ec72b91ae165209edff4208f84
pydev <pydev@MAC19.local>
2013-09-25 12:49:31 -0400

    Initialize

--- push.py
+++ push.py
@@ -2,4 +2,4 @@ import os
 msg = str(raw_input("Please enter a commit reason: "))
 os.system("git add .")
 os.system("git commit -m \"{0}\"".format(msg))
-os.system("git push")
+os.system("git push origin master")
