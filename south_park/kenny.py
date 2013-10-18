ff97e410e92467973be81e00ca20f60460307ca0
pydev <pydev@MAC19.local>
2013-10-18 10:11:11 -0400

    kenny finished

--- south_park/kenny.py
+++ south_park/kenny.py
@@ -7,7 +7,7 @@ class kenny_class(Stuff):
 
     
     def __init__(self, image, a, b):
-        self.kennyImages = ["1.psd", "2.psd", "3.psd", "4.psd", "5.psd", "6.psd", "7.psd", "8.psd", "9.psd", "10.psd"]
+        self.kennyImages = ["1.psd", "2.psd", "3.psd", "4.psd", "5.psd", "6.psd", "7.psd", "8.psd", "9.psd", "10.psd", "dead.psd"]
         self.i = 0
         self.speed = [a, b]
         self.image = image
@@ -15,6 +15,13 @@ class kenny_class(Stuff):
         self.stuffrect = None
         
         self.setup()
+    def change_kenny_image(self):
+        self.image = self.kennyImages[self.i]
+        self.i += 1
+        #self.setup()
+        print self.image
+
+        self.stuff = pygame.image.load(self.image)
     
     def update(self):
         
@@ -23,21 +30,21 @@ class kenny_class(Stuff):
             self.speed[0] = -self.speed[0]
             #Select next image in list
             
-            self.image = self.kennyImages[self.i]
-            self.i += 1
-            #self.setup()
-            print self.image
-            
-            self.stuff = pygame.image.load(self.image)
+            if self.i != (len(self.kennyImages)):
+                self.change_kenny_image()
             
         
         if self.stuffrect.top < 0 or self.stuffrect.bottom > height:
             self.speed[1] = -self.speed[1]
             #Select next image in list
-            self.image = self.kennyImages[self.i]
-            self.i += 1
-            #self.setup()
-            print self.image
             
-            self.stuff = pygame.image.load(self.image)
             
+            if self.i != (len(self.kennyImages)):
+                self.change_kenny_image()
+                
+        
+            
+            
+               # while: 
+                #    self.i = 11
+                 #   print "hello"
